8ae5d120125f1d79f30e361d54b691e2ceed8007
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-07-10 14:18:47 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import *
+
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/")
+def homePage():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
