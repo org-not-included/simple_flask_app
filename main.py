cd8e70694544513908db367e3839369e4e63c836
Mike Sadler <101577043+mtsadler-branch@users.noreply.github.com>
2022-07-10 14:50:18 -0500

    use block content (DRY header)

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def homePage():
-    return render_template("home.html")
+    return render_template("home.html", name="Mike", age=27)
+
+
+@app.route("/second")
+def secondPage():
+    return render_template("secondPage.html")
+
 
 if __name__ == '__main__':
     app.debug = True
